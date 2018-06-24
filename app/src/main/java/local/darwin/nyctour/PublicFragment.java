3e35675a7fe149feefd5d81155fa3fcf8b2a69b6
darwinyip <yipdarwin@gmail.com>
2018-06-24 14:15:01 -0400

    Made it better

--- app/src/main/java/local/darwin/nyctour/PublicFragment.java
+++ app/src/main/java/local/darwin/nyctour/PublicFragment.java
@@ -21,9 +21,9 @@ public class PublicFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         ArrayList<Place> places = new ArrayList<>();
-        places.add(new Place("Central Park", "Manhattan", "Central Park is a visual masterpiece created by landscape designer Frederick Law Olmsted and architect Calvert Vaux. It has gone through major developments and restoration over time to carry on its initial purpose as an open-air oasis for a metropolitan city. No matter the season or reason for your visit, this national historic landmark is a setting for enjoying many pursuits.", R.raw.central_park));
-        places.add(new Place("Bryant Park", "Manhattan", "Bryant Park is a beloved, year-round New York City destination. Known as Manhattan's Town Square, the park is famous for its lush seasonal gardens, free activities, world class restrooms, and al fresco dining. Located adjacent to the New York Public Library and surrounded by iconic skyscrapers, Bryant Park is visited by more than six million people each year and is one of the busiest public spaces in the world.", R.raw.bryant_park));
-        places.add(new Place("Dumbo", "Brooklyn", "Trendy Dumbo's cobblestone streets and converted Brooklyn warehouse buildings are the backdrop for independent boutiques, high-end restaurants and trendy cafes. Near the waterfront, St. Ann's Warehouse, in a former tobacco factory, is the heart of a thriving performance and gallery scene.", R.raw.dumbo));
+        places.add(new Place(getString(R.string.central_park), getString(R.string.mahattan), getString(R.string.centra_park_desc), R.drawable.central_park));
+        places.add(new Place(getString(R.string.bryant_park), getString(R.string.mahattan), getString(R.string.bryant_park_desc), R.drawable.bryant_park));
+        places.add(new Place(getString(R.string.dumbo), getString(R.string.brooklyn), getString(R.string.dumbo_desc), R.drawable.dumbo));
 
 
         RecyclerView recyclerView = rootView.findViewById(R.id.section_container);
